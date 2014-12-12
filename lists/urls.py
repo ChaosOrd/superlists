53256df957b0e89ef494b786b9997a9497949e3b
Pavel Rybintsev <punkihoy86@gmail.com>
2014-12-12 12:10:23 +0200

    Moved urls that belong to lists app to separate file

---
+++ lists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns
+
+
+urlpatterns = patterns('',
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='new_list'),
+)
