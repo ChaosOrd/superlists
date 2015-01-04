68753acfa03b6654083355aee0942246c39b4f51
Pavel Rybintsev <punkihoy86@gmail.com>
2015-01-05 00:30:24 +0200

    Added username to server host in "server_tools"

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,13 +1,14 @@
 from os import path
 import subprocess
 THIS_FOLDER = path.dirname(path.abspath(__file__))
+SERVER_USERNAME = 'ChaosOrd'
 
 
 def create_session_on_server(host, email):
     return subprocess.check_output(
         [
             'fab',
-            'create_session_on_server:email={}'.format(email),
+            'create_session_on_server:email={}@{}'.format(SERVER_USERNAME, email),
             '--host={}'.format(host),
             '--hide=everything,status'
         ],
@@ -17,6 +18,6 @@ def create_session_on_server(host, email):
 
 def reset_database(host):
     subprocess.check_call(
-        ['fab', 'reset_database', '--host={}'.format(host)],
+        ['fab', 'reset_database', '--host={}@{}'.format(SERVER_USERNAME, host)],
         cwd=THIS_FOLDER
     )
